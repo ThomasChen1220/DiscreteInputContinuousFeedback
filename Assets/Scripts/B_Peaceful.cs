590aaa997216c1a69a18b1b6959988f143c259ac
ThomasChen <canchen@usc.edu>
2020-09-30 00:37:37 +0800

    some visual for mating

--- Assets/Scripts/B_Peaceful.cs
+++ Assets/Scripts/B_Peaceful.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class B_Peaceful : SlimeBehavior
 {
+    public Color tint;
     public float mateInterval = 3f;
     public float lastMate;
     protected override void OnStart()
     {
         base.OnStart();
         lastMate = Time.time;
+        gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+    }
+    private void Update()
+    {
+        if(lastMate + mateInterval < Time.time)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = tint;
+        }
     }
     public override void DoMyThing(Collision2D collision) {
         B_Peaceful other = collision.gameObject.GetComponent<B_Peaceful>();
         if (other != null)
         {
-            float nextMateTime = Mathf.Min(lastMate + mateInterval, other.lastMate + other.mateInterval);
+            float nextMateTime = Mathf.Max(lastMate + mateInterval, other.lastMate + other.mateInterval);
             if(nextMateTime < Time.time && lastMate<other.lastMate)
             {
                 //make a baby
@@ -24,6 +33,9 @@ public class B_Peaceful : SlimeBehavior
                 c.transform.position = (transform.position + other.transform.position)/2;
                 lastMate = Time.time;
                 other.lastMate = Time.time;
+
+                gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+                other.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
             }
         }
     }
